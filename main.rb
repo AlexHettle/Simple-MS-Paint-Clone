506c0c96b98fa15ad3b68e0824f9a6da22fd1e52
Alex Hettle <51217513+AlexHettle@users.noreply.github.com>
2020-06-13 21:01:04 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,9 +1,13 @@
 require 'ruby2d'
 set title: "Paint",background: 'white', width: 456, height: 500
+#Starting brush color
 chosen_color="black"
+#startng eraser color
 erase_color="white"
+#list of colors used when user chooses a new color
 color_list=['red','orange','yellow','green','blue','#EE82EE','black','#808080','white']
 draw_size=10
+#creates all buttons for colors
 def create_color_buttons()
   Rectangle.new(x:5,y:450,width:45,height:45,color:'red',z:1)
   Rectangle.new(x:55,y:450,width:45,height:45,color:'orange',z:1)
@@ -14,14 +18,15 @@ def create_color_buttons()
   Rectangle.new(x:305,y:450,width:45,height:45,color:'black',z:1)
   Rectangle.new(x:355,y:450,width:45,height:45,color:'#808080',z:1)
   Rectangle.new(x:405,y:450,width:45,height:45,color:'white',z:1)
-
 end
+#creates border area
 def create_border()
   Rectangle.new(x:430,y:0,width:25,height:500,color:"#d6d4d3",z:1)
   Rectangle.new(x:0,y:0,width:55,height:500,color:"#d6d4d3",z:1)
   Rectangle.new(x:0,y:445,width:460,height:55,color:"#d6d4d3",z:1)
   Rectangle.new(x:0,y:20,width:500,height:25,color:"#d6d4d3",z:1)
 end
+#creates the eraser, paint bucket, increase and decrease buttons
 def create_extra_buttons()
   Rectangle.new(x:5,y:50,width:40,height:40,color:"white",z:1)
   Rectangle.new(x:16,y:66,width:18,height:9,color:"#ff7eec",z:1)
@@ -33,11 +38,14 @@ def create_extra_buttons()
   Rectangle.new(x:5,y:200,width:40,height:40,color:"white",z:1)
   Triangle.new(x1:15,x2:35,x3:25,y1:210,y2:210,y3:230,color:"black",z:1)
 end
+#Creates GUI
 create_border()
 create_color_buttons()
 create_extra_buttons()
+#When mouse is clicked, this chunk of code determines where the click was, and determines
+#how to handle the 
 on :mouse_down  do |event|
-  if event.x>50 && event.x<400 && event.y>15 && event.y<445
+  if event.x>50 && event.x<430 && event.y>15 && event.y<445
     Circle.new(x:event.x,y:event.y,radius:draw_size,color:chosen_color,z:1)
     create_border()
     create_color_buttons()
